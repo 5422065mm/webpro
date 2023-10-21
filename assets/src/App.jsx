ea5b5732878376c190b640cf4cd91203849a52bf
Minato Muramatsu <chmi22065@g.nihon-u.ac.jp>
2023-10-21 18:11:41 +0900

    完成

---
+++ assets/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import Main from "./components/maincon";
+import Footer from "./components/footer";
+
+export default function App() {
+    return(
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+            
+    );
+  }
---
+++ assets/src/components/maincon.jsx
@@ -0,0 +1,11 @@
+import Aside from "./aside";
+import Article from "./article";
+
+export default function maincon(){
+    return(
+        <main>
+            <Article />
+            <Aside />
+        </main>
+    );
+}
